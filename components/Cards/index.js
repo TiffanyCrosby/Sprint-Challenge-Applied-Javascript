5f7ca2cae6abd94bff325ef5cd4ab7272d75851d
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-02-14 13:58:34 -0500

    finished MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,73 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log(response);
+        let javascript = response.data.articles.javascript;
+        let bootstrap = response.data.articles.bootstrap;
+        let technology = response.data.articles.technology;
+        let jquery = response.data.articles.jquery;
+        let node = response.data.articles.node;
+
+        javascript.forEach((obj) => {
+            const cards = article(obj);
+            cardsContainer.appendChild(cards);
+        });
+        bootstrap.forEach((obj) => {
+            const cards = article(obj);
+            cardsContainer.appendChild(cards);
+        });
+        jquery.forEach((obj) => {
+            const cards = article(obj);
+            cardsContainer.appendChild(cards);
+        });
+        node.forEach((obj) => {
+            const cards = article(obj);
+            cardsContainer.appendChild(cards);
+        });
+        technology.forEach((obj) => {
+            const cards = article(obj);
+            cardsContainer.appendChild(cards);
+        });
+    })
+
+    .catch(error => {
+        console.log("Data Not Returned on Cards", error);
+    })
+
+
+function article(obj) {
+
+    //create elements
+    let card = document.createElement('div');
+    let headLine = document.createElement('div');
+    let author = document.createElement('div');
+    let imgContainer = document.createElement('div');
+    let img = document.createElement('img');
+    let authorName = document.createElement('span');
+
+    //add classes
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    //add attributes
+    headLine.textContent = obj.headline;//for now ---replace
+    img.src = obj.authorPhoto; //for now ---replace
+    authorName.textContent = `By ${obj.authorName}`; //for now ---replace
+
+    //append
+    card.appendChild(headLine);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(authorName);
+    imgContainer.appendChild(img);
+
+    return card;
+
+}
+
+let cardsContainer = document.querySelector('.cards-container');
